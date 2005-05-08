b4186760c2d078720fab11b3421d0e1fab2e37ef
Eric Richardson <bit@ericrichardson.com>
2005-05-08 07:36:04 +0000

    Moved Format Object
    * Moved Object::Format::Markdown to Object::System::Format::Markdown
    
    git-archimport-id: e@ericrichardson.com--2004-eThreads/eThreads--eTrevCGI--0.1--patch-10

---
+++ Object/System.pm
@@ -0,0 +1,55 @@
+package eThreads::Object::System;
+
+use strict;
+
+#----------
+
+sub new {
+	my $class = shift;
+	my $data = shift;
+
+	$class = bless ( {
+		_		=> $data,
+	} , $class ); 
+
+	return $class;
+}
+
+#----------
+
+sub load {
+	my $class = shift;
+	my $sys = shift;
+
+	return $class->{_}->new_object("System::" . $sys);
+}
+
+#----------
+
+=head1 NAME
+
+eThreads::Object::System
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+
+=over 4
+
+
+=back
+
+=head1 AUTHOR
+
+Eric Richardson <e@ericrichardson.com>
+
+=head1 COPYRIGHT
+
+Copyright (c) 1999-2005 Eric Richardson.   All rights reserved.  eThreads 
+is licensed under the terms of the GNU General Public License, which you 
+should have received in your distribution.
+	
+=cut
+
+1;
